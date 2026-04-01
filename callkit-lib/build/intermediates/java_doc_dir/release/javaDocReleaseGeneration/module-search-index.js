e3c32cb172ad04b354c7845da61ec9e4d1150e73
Mohammad Ahmadi Sadr <ahmadisadr@icloud.com>
2026-03-31 23:52:50 -0400

    Remove all build artifacts from tracking

--- callkit-lib/build/intermediates/java_doc_dir/release/javaDocReleaseGeneration/module-search-index.js
+++
@@ -1,1 +0,0 @@
-var moduleSearchIndex = [{"l":":callkit-lib","url":"index.html"}]
--- callkit-lib/build/intermediates/java_doc_dir/release/javaDocReleaseGeneration/package-search-index.js
+++
@@ -1,1 +0,0 @@
-var packageSearchIndex = [{"l":"social.persian.callkit_lib","url":"social/persian/callkit_lib/package-summary.html"}, {"l":"All packages","url":"index.html"}]
